035181e7eb394466c50ef4f3195b235def79ecbe
Badieh <badienader42@gmail.com>
2022-08-09 20:23:05 +0200

    changing project name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+
       title: 'Find Job App',
       theme: ThemeData(
         // This is the theme of your application.

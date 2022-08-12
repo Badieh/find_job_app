66136228b1e82edc2c82a31b4f7e5d560a0eb836
BishoyShehata <bishoyshehata9@gmail.com>
2022-08-13 00:17:59 +0200

    add_photos of profile screen

--- lib/modules/personal_screen.dart
+++ lib/modules/personal_screen.dart
@@ -5,6 +5,22 @@ class PersonalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('PersonalScreen '));
+    return Scaffold(
+      appBar: AppBar(
+      backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body:
+      Row(
+        children: [
+          SizedBox(width: 20,),
+          Container(
+            height: 50,
+            child: Text("Profile",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+
+          ),
+        ],
+      ),
+    );
   }
 }

f20d1b8e3554d7d6bba1b0d0d51a353b4d09265f
BishoyShehata <bishoyshehata9@gmail.com>
2022-08-13 01:39:26 +0200

    I have added profile screen

--- lib/modules/home_screen.dart
+++ lib/modules/home_screen.dart
@@ -14,27 +14,29 @@ class HomeScreen extends StatelessWidget {
           Row(
             children: [
               Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Hello Hafiz',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          color: Color(0xff05182A)),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Find your Dream jobs!',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          color: Color(0xff05182A)),
-                    ),
-                  ],
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Hello Hafiz',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xff05182A)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Find your Dream jobs!',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xff05182A)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Container(
--- lib/modules/personal_screen.dart
+++ lib/modules/personal_screen.dart
@@ -6,20 +6,259 @@ class PersonalScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body:
-      Row(
-        children: [
-          SizedBox(width: 20,),
-          Container(
-            height: 50,
-            child: Text("Profile",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
+      body: SingleChildScrollView(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 50,
+                  child: Text(
+                    "Profile",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    child: Text(
+                      "                        ...",
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset("assets/images/back_profile.png"),
+                Positioned(
+                  top: 0,
+                  child: Image.asset("assets/images/profile.png"),
+                ),
+                Positioned(
+                  top: 80,
+                  child: Container(
+                    height: 50,
+                    child: Text(
+                      "Hafizdzaki",
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 105,
+                  child: Container(
+                    height: 50,
+                    child: Text(
+                      "Senior UI/UX Designer",
+                      style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                ),
+                Positioned(
+                    top: 130,
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "31",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Applied",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "17",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Reviewed",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "5",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Contacted",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                SizedBox(width: 20,),
+                Text(
+                  "ACCOUNT",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 380,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      Image.asset("assets/images/personal.png"),
+                      SizedBox(width: 15,),
+                      Text(
+                        "Personal Data",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios),
 
-          ),
-        ],
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+
+            Container(
+              width: 380,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      Image.asset("assets/images/resume.png"),
+                      SizedBox(width: 15,),
+                      Text(
+                        "Resume & My info",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios),
+
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+
+            Container(
+              width: 380,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      Image.asset("assets/images/my_app.png"),
+                      SizedBox(width: 15,),
+                      Text(
+                        "My Application",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios),
+
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                SizedBox(width: 20,),
+                Text(
+                  "Other",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+
+            Container(
+              width: 380,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      Image.asset("assets/images/setting.png"),
+                      SizedBox(width: 15,),
+                      Text(
+                        "Settings",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios),
+
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+
+          ],
+        ),
       ),
     );
   }
